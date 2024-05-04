a16aec32fd65b173424c3a6c6a4e521737a2d8d3
John Rufo de la Peña <jodelapena2@up.edu.ph>
2024-05-04 21:21:26 +0800

    Set up server middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,30 @@ if (isNaN(port)) {
     throw new Error("PORT environment variable is not a number");
 }
 
+// Add request console logger
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request: ${req.url}`);
+    console.log(`Origin: ${req.headers.origin}`);
+    console.log(`Authorization: ${req.headers.authorization}`);
+    console.log(`Body: ${typeof req.body === 'object' ? JSON.stringify(req.body) : req.body}`);
+
+    next();
+});
+
+// Set up CORS middleware
+app.use((req, res, next) => {
+    let frontendPort = 3000;
+    let allowedOrigins = [`http://localhost:${frontendPort}`];
+
+    if (req.headers.origin && allowedOrigins.includes(req.headers.origin)) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Credentials', "true");
+        res.setHeader('Access-Control-Allow-Methods', "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
+    }
+
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
