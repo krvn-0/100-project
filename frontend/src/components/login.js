45fffd14ef5810c7eb1e21af15f67c8b015082c2
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-13 03:14:34 +0800

    feat/ui: added a login page with basic form functionalities

---
+++ frontend/src/components/login.js
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+export default function Login(props){
+    const login = props.list;
+
+    const [selectedLogOpt, setSelectedLogOpt] = useState(null);
+
+    const renderFormFields = () => {
+        switch(selectedLogOpt) {
+            case 1:
+                return (
+                    <>
+                      <label>
+                            First Name:
+                            <input type="text" name="fname" className="common_input" required />
+                        </label>
+                        <label>
+                            Middle Name:
+                            <input type="text" name="mname" className="common_input" />
+                        </label>
+                        <label>
+                            Last Name:
+                            <input type="text" name="lname" className="common_input" required />
+                        </label>
+                        <label>
+                            Username:
+                            <input type="text" name="username" className="common_input" required />
+                        </label>
+                        <label>
+                          Email:
+                          <input type="email" name="email" className="common_input" required />
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" className="common_input" required />
+                        </label>
+                        <div className="checkboxInput">
+                          <span>Signing up as a seller?</span>
+                          <input type="checkbox" name="isMerchant" />
+                        </div>
+                        <input type="submit" value="Submit" className="submit" />
+                    </>
+                );
+            case 2:
+                return (
+                    <>
+                        <label>
+                            Email:
+                            <input type="email" name="email" className="common_input" required />
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" className="common_input" required />
+                        </label>
+                        <input type="submit" value="Submit" className="submit"/>
+                    </>
+                );
+            default:
+                return (
+                  <>
+                    <p>
+                      Choose a login option
+                    </p>
+                  </>
+                );
+        }
+    };
+
+    return (
+        <div className="login">
+          <div className="left_login">
+            <div className="App_name_login">
+              <p>Farm-2-U</p>
+            </div>  
+          </div>
+          <div className="right_login">
+            <div className="login_opt">
+              {login.map(log_opt => (
+                <h3 key={log_opt.id} id={log_opt.name.toLowerCase()} 
+                onClick={() => setSelectedLogOpt(log_opt.id)}
+                className={selectedLogOpt === log_opt.id ? 'login_opt_selected' : ''
+                }> 
+                  {log_opt.name}
+                </h3>
+              ))}
+            </div>
+            <form className="login_form">
+              {renderFormFields()}
+            </form>
+          </div>
+        </div>
+    )
+};
--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -2,6 +2,10 @@ export default function Menus(props){
     const menus = props.list;
 
     return (
+      <div className="topnav">
+        <div className="App_name">
+          <p>Farm-2-U</p>
+        </div>
         <div className="select">
           {menus.map(menu => ( // mapping elements of the menus array
             <h3 key={menu.id} id={menu.name.toLowerCase()}> 
@@ -9,6 +13,8 @@ export default function Menus(props){
             </h3>
           ))}
         </div>
+      </div>
+        
     )
 };
     
