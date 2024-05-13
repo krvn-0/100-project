5a9ccfe4917050c9f88c49d67837ca4074060a31
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-13 23:29:02 +0800

    chore/test: added placeholder text for the homepage

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -7,13 +7,22 @@ export default function Home(props){
     
     return (
       <div className="home_page">
+        <div className="home_contents">
         <div className="greeting">
             <p>Hello, {username}</p>
         </div>
+        <div className="instruction">
+            <p className="message">
+                Welcome to Farm-2-U, an app made to make buying and 
+                selling agricultural products easier for both consumer
+                and producer
+            </p>
+        </div>
         <div>
-            <button onClick={handleLogout}>
-                logout
+            <button onClick={handleLogout} className="logout">
+                Logout
             </button>
+        </div>
         </div>
       </div>
     )
