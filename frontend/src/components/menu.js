d7acc19aaaf8d5f32c1402d4ad05611ceca7a957
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-13 23:04:51 +0800

    feat/ui: added some blocks of code essential in determining what html elements App.js needs to show

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,36 +1,37 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Menus(props){
-    const menus = props.list;
-    const [selectedMenu, setSelectedMenu] = useState(null);
+  const menus = props.list;
+  const [selectedMenu, setSelectedMenu] = useState(null);
 
-    useEffect(() => {
-      const homeMenu = menus.find(menu => menu.name.toLowerCase() === 'home');
-      if (homeMenu) {
-          setSelectedMenu(homeMenu.id);
-      }
+  // sets the default menu to home
+  useEffect(() => {
+    const homeMenu = menus.find(menu => menu.name.toLowerCase() === 'home');
+    if (homeMenu) {
+        setSelectedMenu(homeMenu.id);
+        props.handleSelectClick(homeMenu.id)
+    }
   }, [menus]);
 
-    const handleSelectClick = (menu_id) => {
-      setSelectedMenu(menu_id);
-    }
+  const handleSelectClick = (menu_id) => {
+    setSelectedMenu(menu_id);
+    props.handleSelectClick(menu_id); // Notify the parent component
+  }
 
-    return (
-      <div className="topnav">
-        <div className="App_name">
-          <p>Farm-2-U</p>
-        </div>
-        <div className="select">
-          {menus.map(menu => ( // mapping elements of the menus array
-            <h3 key={menu.id} id={menu.name.toLowerCase()} 
-            onClick={() => handleSelectClick(menu.id)}
-            className={selectedMenu === menu.id ? 'selected_menu' : ''
-                }> 
-              {menu.name}
-            </h3>
-          ))}
-        </div>
+  return (
+    <div className="topnav">
+      <div className="App_name">
+        <p>Farm-2-U</p>
       </div>
-    )
+      <div className="select">
+        {menus.map(menu => (
+          <h3 key={menu.id} id={menu.name.toLowerCase()} 
+          onClick={() => handleSelectClick(menu.id)}
+          className={selectedMenu === menu.id ? 'selected_menu' : ''}> 
+            {menu.name}
+          </h3>
+        ))}
+      </div>
+    </div>
+  )
 };
-    
