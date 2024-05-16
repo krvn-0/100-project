8067e7ad17efdb5f816d20595967709c2d6d9f78
John Rufo de la Peña <jodelapena2@up.edu.ph>
2024-05-16 15:54:27 +0800

    feat: Create UserModel

--- backend/src/entities/user.ts
+++ backend/src/entities/user.ts
@@ -1,4 +1,17 @@
+import { Types } from "mongoose";
 import { Product } from "./product.js";
+
+export type UserDAO = {
+    _id?: Types.UUID;
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+    isMerchant: boolean;
+    productIds: Types.UUID[];
+};
 
 export type UserDOT = {
     id: string;
