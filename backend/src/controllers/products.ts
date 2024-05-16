9175eb256bf0c48b2d91948c2f027960c2dd2417
Rance116 <kvbarral@up.edu.ph>
2024-05-16 14:42:18 +0800

    feat: addProduct controller

--- backend/src/controllers/products.ts
+++ backend/src/controllers/products.ts
@@ -13,10 +13,25 @@ export function addProduct(req: Request, res: Response) {
     const quantity: number = req.body.quantity;
     const unitPrice: number = req.body.unitPrice;
 
-    // const type: ProductType = req.body.type;
-    // const quantity: number = req.body.quantity;
-    // const unitPrice: number = req.body.unitPrice;
-    // const owner: User = req.body.owner;
+    if(typeof(id) !== 'string' || typeof(name) !== 'string' || typeof(description) !== 'string' || typeof(quantity) !== 'number' || typeof(unitPrice) !== 'number' || typeof(ownerId) !== 'string'){
+
+        res.status(400).send({
+            type: "urn:100-project:error:malformed",
+            name: "malformed request",
+            status: 400,
+            message: "id, name, description must be strings and  quantity, unitPrice must be numbers",
+        });
+    }
+    
+    if(type !== ProductType.CROP && type !== ProductType.POULTRY) {
+        res.status(400).send({
+            type: "urn:100-project:error:malformed",
+            name: "malformed request",
+            status: 400,
+            message: "type should be at least 1 or 2"
+            
+        })
+    }
 
     products.push({
         id: id,
@@ -27,4 +42,19 @@ export function addProduct(req: Request, res: Response) {
         quantity: quantity,
         unitPrice: unitPrice
     });
+}
+
+export function getProducts(req: Request, res: Response) {
+    let id: string = req.params.id; // route /:id
+
+    const foundProduct = products.find((p) => p.id == id);
+
+    if(foundProduct === undefined) {
+        res.send({
+            type:"urn:100-project:error:not-found",
+            name: "Product Not Found",
+            status: 404,
+            message: "Product does not exist"
+        });
+    }
 }
