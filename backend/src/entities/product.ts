2e0b0f34b8b003ab309d27f9b2512e2d6a94376d
John Rufo de la Peña <jodelapena2@up.edu.ph>
2024-05-20 17:47:24 +0800

    feat: Create ProductModel

--- backend/src/entities/product.ts
+++ backend/src/entities/product.ts
@@ -1,9 +1,20 @@
+import { Types } from "mongoose";
 import { User } from "./user.js";
 
 export enum ProductType {
     CROP = 1,
     POULTRY = 2,
 };
+
+export type ProductDAO = {
+    _id: Types.UUID;
+    name: string;
+    description: string;
+    ownerId: Types.UUID;
+    type: ProductType;
+    quantity: number;
+    unitPrice: number;
+}
 
 export type ProductDOT = {
     id: string;
