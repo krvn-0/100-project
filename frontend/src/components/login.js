a2028d03a0567b552da2879b029739d01a378696
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-22 16:49:31 +0800

    fix: added missing submit button

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -79,6 +79,7 @@ export default function Login(props) {
             <div className="showPass">
               <span>Show password</span>
             </div>
+            <input type="submit" value="Submit" className="submit" />
           </>
         )
       default:
