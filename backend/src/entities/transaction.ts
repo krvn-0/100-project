0af4ab24421d05fccaf1cf3efdf24249feedc254
krvn-0 <kasamson@up.edu.ph>
2024-05-23 13:50:20 +0800

    feat: create Transaction schema

---
+++ backend/src/entities/transaction.ts
@@ -0,0 +1,25 @@
+import 'Types' from 'mongoose';
+import {User} from './user.js';
+import {Product} from './product.js';
+ 
+export enum TransactionStatus = {
+    CANCELLED = -1,
+    PENDING = 0,
+    CONFIRMED = 1
+}
+
+export type Transaction = {
+    user: User,
+    product: Product,
+    quantity: number,
+    status: TransactionStatus,
+    timestamp: number
+}
+
+export type TransactionDAO = {
+    user: Types.UUID,
+    product: Types.UUID,
+    quantity: Number,
+    status: TransactionStatus,
+    timestamp: Date
+}
--- backend/src/entities/user.ts
+++ backend/src/entities/user.ts
@@ -38,3 +38,4 @@ export type UserToken = {
     id: string,
     isAdmin: true,
 };
+
