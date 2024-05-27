f448760d980fc6187c5fff8ca4a4cbc5732b1c75
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-27 23:36:33 +0800

    chore: added possible routing for the different components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter} from 'react-router-dom';
 
 import App from './App';
+import UserDashboard from './dashboards/UserDashboard';
+import AdminDashboard from './dashboards/AdminDashboard';
+import Home from './components/home';
+import ItemList from './user/items';
+import CartList from './user/cart';
+import OrderList from './user/order';
+import Login from './components/login';
+import AdminUser from './admin/adminUser';
+import AdminProduct from './admin/adminProduct';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/user',
+//         element: <UserDashboard />,
+//         children: [
+//           {
+//             path: '/user/home',
+//             element: <Home />,
+//           },
+//           {
+//             path: 'user/items',
+//             element: <ItemList />,
+//           },
+//           {
+//             path: 'user/cart/:userID',
+//             element: <CartList />,
+//           },
+//           {
+//             path: 'user/order/:userID',
+//             element: <OrderList />,
+//           }
+//         ],
+//       },
+//       {
+//         path: '/admin',
+//         element: <AdminDashboard />,
+//         children: [
+//           {
+//             path: '/admin/users',
+//             element: <AdminUser />,
+//           },
+//           {
+//             path: 'admin/items',
+//             element: <AdminProduct />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     path: '/login',
+//     element: <Login />
+//   }
+// ])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
