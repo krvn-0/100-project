0dfc63833fa8c8a77d3c1fc9f0829a82a21e9fc6
aguitezFRG <fpaguitez@up.edu.ph>
2024-06-05 22:51:49 +0800

    chore: overhauled specific item view

--- frontend/src/popups/ViewProduct.js
+++ frontend/src/popups/ViewProduct.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './Popup.css';
 import './ViewProduct.css';
 
 const ViewProductPopup = ({ product, handleAddClick, handleClose }) => {
     return (
         <div className='popup'>
             <div className={`popup-inner ${product.type === 1 ? 'crops' : 'animals'} `}>
-                <h2 className="product-name">{product.name}</h2>
-                <p className="product-description">{product.description}</p>
-                <p className="product-price">Price: P{product.price} per {product.qty_type}</p>
-                <p className="product-stock">Stock: {product.quantity} {product.qty_type}</p>
+                <div className="product-image"><img src={product.image} alt={product.name} /></div>
+                <div className='product-info'>
+                    <h2 className="product-name">{product.name}</h2>
+                    <p className='product-type'>{product.type === 1 ? 'Crop' : 'Animal'}</p>
+                    <p className="product-description">{product.description}</p>
+                    <p className="product-price">Price: P{product.price} per {product.qty_type}</p>
+                    <p className="product-stock">Stock: {product.quantity} {product.qty_type}</p>
+                </div>
                 <div className="popup-buttons">
                     <button className='add-to-cart' onClick={() => handleAddClick(product, 1)}>Add</button>
                     <button className='close' onClick={handleClose}>Close</button>
