a5495a06877cdabceea3c886e60ffc4ec6f50266
krvn-0 <kasamson@up.edu.ph>
2024-06-05 15:02:53 +0800

    chore: add routes

--- backend/src/routers.ts
+++ backend/src/routers.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 
 import { createUser, getUser, getUsers, login, logout } from "./controllers/internal_data.js";
 import { deleteUser, updateUser } from "./controllers/internal_data/users.js";
+import { createTransaction, getTransactions, updateTransaction } from "./controllers/mongodb/transactions.js";
 
 export function RouteWithInternalData(app: Express) {
     app.post('/login', login);
@@ -12,4 +13,8 @@ export function RouteWithInternalData(app: Express) {
     app.get("/users/:id", getUser);
     app.patch("/users/:id", updateUser);
     app.delete("/users/:id", deleteUser);
+
+    app.get("/transactions", getTransactions);
+    app.put("/transactions", createTransaction);
+    app.patch("/transactions/:id", updateTransaction);
 }
