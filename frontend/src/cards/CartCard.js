80d026d6261384d5f9c8a09cd61d927ad47bedc2
aguitezFRG <fpaguitez@up.edu.ph>
2024-06-06 13:43:31 +0800

    chore: initial progress for cart overhaul

---
+++ frontend/src/cards/CartCard.js
@@ -0,0 +1,18 @@
+import './CartCard.css';
+
+
+const CartCard = ({ product, handleRemoveClick }) => {
+    return (
+        <div className="cart-card">
+            {/* <div className="cart-card-image">
+                <img src={product.image} alt={product.name} />
+            </div> */}
+            <div className="cart-card-info">
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+                <p>${product.price}</p>
+                <button onClick={handleRemoveClick}>Remove</button>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './CartPage.css';
 
-const Cart = () => {
+const CartPage = () => {
+    const [cartDetails, setCartDetails] = useState([]);
     const [cartItems, setCartItems] = useState([]);
+
+    // const extractProductinCart = async (cartDetails) => {
+    //     const products = await Promise.all(cartDetails.map(async (item) => {
+    //         const response = await fetch(`http://localhost:3001/products/${item.productID}`);
+    //         const data = await response.json();
+    //         return {
+    //             id: data.productID,
+    //             name: data.name,
+    //             type: product.type,
+    //             price: data.price,
+    //             qty_type: data.qty_type,
+    //             quantity: item.quantity
+    //         }
+    //     }));
+    //     setCartItems(products);
+    // }
+
+
+    // useEffect(() => {
+    //     try {
+    //         fetch(`http://localhost:3001/user/${userID}`)
+    //         .then((response) => response.json())
+    //         .then((data) => setCartDetails(data.cart));
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    //     extractProductinCart(cartDetails);
+    // }, [cartDetails])
+
+    // const updateUser = async (userID, updatedCart) => {
+    //     try {
+    //         const response = await fetch(`http://localhost:3001/user/${userID}`, {
+    //             method: 'PUT',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({products: updatedCart})
+    //         });
+    //         const data = await response.json();
+    //         serCartDetails(data.cart);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    const updateUserCart = (product) => {
+        // const updatedCart = cartDetails.filter((item) => item.id !== product.id);
+        // // updateUser(userID, updatedCart);
+        // setCartDetails(updatedCart);
+    }
+
+    const handleRemoveClick = (product) => {
+        updateUserCart(product);
+    }
+
+    const handleOrderClick = (product) => {
+        updateUserCart(product);
+    }
+
+    const renderItems = cartItems.map((product) => {
+        return (
+            <CartCard
+                key={product.id}
+                product={product}
+                handleRemoveClick={() => handleRemoveClick(product)}
+            />
+        )
+    })
+
+    return (
+        <div className="cart-page">
+            <h1 className="cart-header">Shopping Cart</h1>
+            <div className="cart-list">
+                {renderItems}
+            </div>
+        </div>
+    )
+
 }
 
-export default Cart;
+export default CartPage;
